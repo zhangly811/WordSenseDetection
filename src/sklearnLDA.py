7429f782254a18f717a1b3c2d11527030df59ffb
zhangly811 <zhangly811@gmail.com>
2019-11-20 01:06:13 -0500

    add sklearn LDA model

---
+++ src/sklearnLDA.py
@@ -0,0 +1,111 @@
+from sklearn.decomposition import LatentDirichletAllocation
+import os
+import numpy as np
+import pandas as pd
+import pickle
+from absl import flags
+from absl import app
+
+flags.DEFINE_integer(
+    "num_topics",
+    default=3,
+    help="The number of topics.")
+flags.DEFINE_integer(
+    "num_top_words",
+    default=10,
+    help="The number of most likely words to display in a topic.")
+flags.DEFINE_string(
+    "master_data_dir",
+    default="/Users/linyingzhang/git/zhangly811/WordSenseDetection/dat_unsync/output/",
+    help="Directory where data is stored (if using real data).")
+flags.DEFINE_string(
+    "word",
+    default="cold",  # "/tmp/lda/data"
+    help="Target word to disambiguate.")
+flags.DEFINE_string(
+    "source",
+    default="MSH",  # "/tmp/lda/data"
+    help="The data source.")
+flags.DEFINE_string(
+    "model_dir",
+    default="/Users/linyingzhang/git/zhangly811/WordSenseDetection/res",  # "/tmp/lda/",
+    help="Directory to put the model's fit.")
+
+FLAGS = flags.FLAGS
+FILE_TEMPLATE = "{split}.npy"
+
+def load_dataset(directory, split_name):
+    data = np.load(os.path.join(directory, FILE_TEMPLATE.format(split=split_name)), allow_pickle=True)
+    data = np.array([list(word) for word in data], dtype=np.float32)
+    return data
+
+#
+# FLAGS.master_data_dir = "/Users/linyingzhang/git/zhangly811/WordSenseDetection/dat_unsync/output/"
+# FLAGS.word = "ventricle"
+# FLAGS.source = "MSH"
+# FLAGS.num_topics = 2
+
+def main(argv):
+    del argv  # unused
+
+    data_dir = os.path.join(FLAGS.master_data_dir, "{}_{}_window10".format(FLAGS.word, FLAGS.source))
+
+    with open(os.path.join(data_dir, "dict.pkl"), "rb") as f:
+        words_to_idx = pickle.load(f)
+    num_words = len(words_to_idx)
+
+    vocabulary = [None] * num_words
+    for word, idx in words_to_idx.items():
+        vocabulary[idx] = word
+
+    dataset_train = load_dataset(data_dir, "train")
+    dataset_test = load_dataset(data_dir, "test")
+    dataset = np.concatenate((dataset_train, dataset_test), axis=0)
+
+    lda = LatentDirichletAllocation(n_components=FLAGS.num_topics, random_state=7)
+    lda.fit(dataset)
+
+    # get topics for some given samples:
+    topic_prob_per_doc = lda.transform(dataset)
+    topic_assignment_per_doc = topic_prob_per_doc.argmax(axis=1)
+
+    top_words_idx = np.array([topic[::-1] for topic in lda.components_.argsort(axis=1)])[:,:FLAGS.num_top_words]
+    top_words_in_topics = []
+
+    for topic in range(FLAGS.num_topics):
+        top_words_in_topics.append(np.array([vocabulary[idx] for idx in top_words_idx[topic,:]]).reshape(1,-1))
+    top_words_in_topics = np.array(top_words_in_topics).reshape(FLAGS.num_topics, FLAGS.num_top_words)
+    for topic in range(FLAGS.num_topics):
+        print("Topic {}: {}".format(topic, top_words_in_topics[topic,:]))
+    # evaluation
+    if FLAGS.source == "MSH" and FLAGS.word == "ventricle":
+        trainlabels = np.loadtxt(os.path.join(data_dir, "train.labels"),dtype=np.str)
+        testlabels = np.loadtxt(os.path.join(data_dir, "test.labels"), dtype=np.str)
+
+        labels = np.concatenate((trainlabels, testlabels))
+
+        labels = [label.replace('M1', '1') for label in labels] #M1 is heart
+        labels = [label.replace('M2', '0') for label in labels] #M2 is brain
+        labels = np.array(labels).astype(int)
+
+        print ("Accuracy: ", np.mean(topic_assignment_per_doc == labels))
+
+    if FLAGS.source == "MSH" and FLAGS.word == "cold":
+        trainlabels = np.loadtxt(os.path.join(data_dir, "train.labels"),dtype=np.str)
+        testlabels = np.loadtxt(os.path.join(data_dir, "test.labels"), dtype=np.str)
+
+        labels = np.concatenate((trainlabels, testlabels))
+
+        labels = [label.replace('M1', '0') for label in labels] #M1 is low temp
+        labels = [label.replace('M2', '1') for label in labels] #M2 is COLD
+        labels = [label.replace('M3', '2') for label in labels] #M3 is common cold
+
+        labels = np.array(labels).astype(int)
+
+        print ("Accuracy: ", np.mean(topic_assignment_per_doc == labels))
+
+
+
+if __name__ == "__main__":
+    app.run(main)
+
--- src/test.py
+++ src/test.py
@@ -45,117 +45,28 @@ def PubMed_dataset(directory, split_name, num_words):
       (np.ones(indices.shape[0]), (indices[:, 0], indices[:, 1])),
       shape=(num_documents, num_words),
       dtype=np.float32)
-  sparse_matrix = sparse_matrix.tocsr()
+  sparse_matrix = sparse_matrix.todense()
   return sparse_matrix
 
-directory = "/Users/linyingzhang/git/zhangly811/WordSenseDetection/dat_unsync/data"
-FILE_TEMPLATE = "{split}.txt.npy"
 
-with open(download(directory, "vocab.pkl"), "rb") as f:
+#######################
+# Change the directory
+directory = "/Users/linyingzhang/git/zhangly811/WordSenseDetection/dat_unsync/output/ventricle_MSH/"
+#######################
+FILE_TEMPLATE = "{split}.npy"
+
+
+
+with open(download(directory, "dict.pkl"), "rb") as f:
     words_to_idx = pickle.load(f)
-num_words = len(words_to_idx)
+    num_words = len(words_to_idx)
 
-vocabulary = [None] * num_words
-for word, idx in words_to_idx.items():
-    vocabulary[idx] = word
+    vocabulary = [None] * num_words
+    for word, idx in words_to_idx.items():
+        vocabulary[idx] = word
 
-sparse_matrix = PubMed_dataset(directory, 'train', num_words)
-X = sparse_matrix.todense()
-
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-from sklearn.decomposition import PCA
-pca = PCA(n_components=50)
-pca.fit(X.T)
-toppcs=pca.components_[:7]
-print(pca.explained_variance_)
-
-
-
-#### tSNE
-from sklearn.manifold import TSNE
-tsne = TSNE(n_components=2)
-X_2d = tsne.fit_transform(toppcs.T)
-y = np.array([x.strip() for x in open("/Users/linyingzhang/git/zhangly811/WordSenseDetection/dat_unsync/data/cold_train.labels", "rt")])
-y[y=="M1"]=0
-y[y=="M2"]=1
-y[y=="M3"]=2
-y = np.array(y, dtype=int)
-target_names = ['Cold Temp', 'COLD', 'the Common Cold']
-target_ids = [0,1,2]
-
-plt.figure(figsize=(6, 5))
-colors = 'r','b','y'
-for i, c, label in zip(target_ids, colors, target_names):
-    plt.scatter(X_2d[y == i, 0], X_2d[y == i, 1], c=c, label=label)
-plt.legend()
-plt.show()
-
-
-from sklearn.cluster import KMeans
-import numpy as np
-kmeans = KMeans(n_clusters=3, random_state=0).fit(toppcs.T)
-kmeans.labels_
-
-bool = [y[i]==kmeans.labels_[i] for i in range(y.shape[0])]
-
-acc = sum(bool)/len(bool)
-acc
-##################################
-# Natural Language Toolkit: Word Sense Disambiguation Algorithms
-#
-# Authors: Liling Tan <alvations@gmail.com>,
-#          Dmitrijs Milajevs <dimazest@gmail.com>
-#
-# Copyright (C) 2001-2019 NLTK Project
-# URL: <http://nltk.org/>
-# For license information, see LICENSE.TXT
-
-from nltk.corpus import wordnet
-
-
-def lesk(context_sentence, ambiguous_word, pos=None, synsets=None):
-    """Return a synset for an ambiguous word in a context.
-
-    :param iter context_sentence: The context sentence where the ambiguous word
-         occurs, passed as an iterable of words.
-    :param str ambiguous_word: The ambiguous word that requires WSD.
-    :param str pos: A specified Part-of-Speech (POS).
-    :param iter synsets: Possible synsets of the ambiguous word.
-    :return: ``lesk_sense`` The Synset() object with the highest signature overlaps.
-
-    This function is an implementation of the original Lesk algorithm (1986) [1].
-
-    Usage example::
-
-        >>> lesk(['I', 'went', 'to', 'the', 'bank', 'to', 'deposit', 'money', '.'], 'bank', 'n')
-        Synset('savings_bank.n.02')
-
-    [1] Lesk, Michael. "Automatic sense disambiguation using machine
-    readable dictionaries: how to tell a pine cone from an ice cream
-    cone." Proceedings of the 5th Annual International Conference on
-    Systems Documentation. ACM, 1986.
-    http://dl.acm.org/citation.cfm?id=318728
-    """
-
-    context = set(context_sentence)
-    if synsets is None:
-        synsets = wordnet.synsets(ambiguous_word)
-
-    if pos:
-        synsets = [ss for ss in synsets if str(ss.pos()) == pos]
-
-    if not synsets:
-        return None
-
-    _, sense = max(
-        (len(context.intersection(ss.definition().split())), ss) for ss in synsets
-    )
-
-    return sense
-
-for ss in wordnet.synsets('tolerance'):
-    print(ss, ss.definition())
+train = PubMed_dataset(directory, 'train', num_words)
+test = PubMed_dataset(directory, 'test', num_words)
+dataset = np.concatenate((train, test), axis=0)
+print("The number of abstract contraining the word:")
+indToKeep=((dataset!=0).sum(axis=0)>=5)
